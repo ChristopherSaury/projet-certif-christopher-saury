64b399e46f5ed23a6202d3b94ec211220920ea84
ChristopherSaury <saurychristopher@gmail.com>
2021-10-05 11:09:31 +0200

    hero banner and DOM modification

--- public/asset/js/home.js
+++ public/asset/js/home.js
@@ -1,4 +1,26 @@
 // PARTIE REVEAL ON SCROLL
+let navHome = document.getElementById('site-header');
+let navItem = document.querySelectorAll('.navigation-link');
+
+window.addEventListener('scroll', ()=>{
+  const scrollPosition = window.scrollY;
+  if(scrollPosition < 80){
+    navHome.style.backgroundColor = 'transparent';
+    navHome.style.boxShadow = 'none';
+    for (let i = 0; i < navItem.length; i++){
+      navItem[i].style.color = '#ffffff';
+      navItem[i].style.hover.color = '#ffffff';
+    }
+  }else if(scrollPosition >= 80){
+    navHome.style.backgroundColor = '#ffffff';
+    navHome.style.boxShadow = '0 0.5rem 1.5rem rgba(0, 0, 0, 0.1)';
+    for (let i = 0; i < navItem.length; i++){
+      navItem[i].style.color = '#684f40';
+    }
+  }
+ });
+
+
 
 window.addEventListener("scroll", reveal);
 
@@ -16,6 +38,7 @@ function reveal() {
     }
   }
 }
+
 
 let currentYear = new Date().getUTCFullYear();
 document.getElementById("currentYear").innerHTML = currentYear;
